ebefdb751d6548009d10f6714d011a9116fb17c6
Sisyphean James <122086282+SisypheanHUS@users.noreply.github.com>
2023-12-02 22:13:28 +0700

    update test pull

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
 public class Hello World{
     public static void main(String args[]){
         System.out.println("hello world nhe");
+        System.out.println("hello world nheaaaaaaaaaaaaaa");
     }
 }
